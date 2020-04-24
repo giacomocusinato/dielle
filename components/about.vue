45d00bf66cfee6321aad7b5656f794a1846810b6
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2020-04-24 17:36:36 +0200

    Add foter info

--- components/about.vue
+++ components/about.vue
@@ -5,21 +5,38 @@
         <h3 class="about__header">La nostra storia</h3>
         <spacer />
         <div class="about__text">
-          <p>DIELLE S.R.L., un’azienda specializzata nella costruzione di quadri elettrici di comando, cablaggi bordomacchina ed automazioni industriali anche della massima sofisticazione.</p>
           <p>
-            L’azienda nasce nel 1989 e i soci titolari hanno già maturato notevole esperienza
-            in aziende del settore di primaria importanza.
-            Il gruppo aziendale e costituito da circa 20 persone con notevole esperienza nella progettazione
-            elettrica, nella costruzione di quadri elettrici anche della massima complessità, nei cablaggi
-            elettrici bordomacchina, del collaudo degli impianti stessi, nella costruzione e nello sviluppo
-            del software sia di automazione che di supervisione.
+            DIELLE S.R.L., un’azienda specializzata nella costruzione di quadri
+            elettrici di comando, cablaggi bordomacchina ed automazioni
+            industriali anche della massima sofisticazione.
           </p>
           <p>
-            La DIELLE SRL collabora ed è fornitrice di numerose aziende costruttrici di macchinari delle più svariate tipologie e dei più svariati settori, dal settore del food a quello del marmo, dal settore del packing a quello delle macchine utensili, dal settore della movimentazione a quello della plastica.
-            L’attività si svolge in un fabbricato di proprietà di 1200 mq completo di carroponte per la movimentazione anche di quadri elettrici di grosse dimensioni.
-            Ai nostri clienti forniamo quadri elettrici, impianti bordomacchina e cablaggi sia su progetto e disegno del committente che su progetto e disegno di nostro sviluppo
+            L’azienda nasce nel 1989 e i soci titolari hanno già maturato
+            notevole esperienza in aziende del settore di primaria importanza.
+            Il gruppo aziendale e costituito da circa 20 persone con notevole
+            esperienza nella progettazione elettrica, nella costruzione di
+            quadri elettrici anche della massima complessità, nei cablaggi
+            elettrici bordomacchina, del collaudo degli impianti stessi, nella
+            costruzione e nello sviluppo del software sia di automazione che di
+            supervisione.
           </p>
-          <p>Le nostre esperienze e le nostre conoscenze ci permettono di eseguire quadri elettrici e cablaggi nel rispetto delle più severe normative, sia europee (CE) che nord – americane (UL/CSA).</p>
+          <p>
+            La DIELLE SRL collabora ed è fornitrice di numerose aziende
+            costruttrici di macchinari delle più svariate tipologie e dei più
+            svariati settori, dal settore del food a quello del marmo, dal
+            settore del packing a quello delle macchine utensili, dal settore
+            della movimentazione a quello della plastica. L’attività si svolge
+            in un fabbricato di proprietà di 1200 mq completo di carroponte per
+            la movimentazione anche di quadri elettrici di grosse dimensioni. Ai
+            nostri clienti forniamo quadri elettrici, impianti bordomacchina e
+            cablaggi sia su progetto e disegno del committente che su progetto e
+            disegno di nostro sviluppo
+          </p>
+          <p>
+            Le nostre esperienze e le nostre conoscenze ci permettono di
+            eseguire quadri elettrici e cablaggi nel rispetto delle più severe
+            normative, sia europee (CE) che nord – americane (UL/CSA).
+          </p>
         </div>
       </div>
       <img class="about__image" src="~assets/images/robot.png" />
@@ -44,7 +61,6 @@ export default {
   display: flex;
 }
 .about__main {
-  flex: 60%;
 }
 .about__header {
   @include section-header;
@@ -54,5 +70,11 @@ export default {
 }
 .about__image {
   width: 300px;
+  margin-left: 30px;
+  display: none;
+
+  @include md {
+    display: block;
+  }
 }
 </style>
--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,24 @@
 <template>
   <footer class="footer">
     <img class="footer__logo" src="~assets/icons/logo-white.svg" />
+    <div class="footer__entries">
+      <div class="footer__entry">
+        <h4 class="footer__entry__header">Contatti</h4>
+        <div class="footer__entry__text">
+          Via Galileo Galilei 1, Galliera Veneta (PD) 35015
+          <br />
+          049548291
+          <br />
+          info@dielleimpianti.it
+        </div>
+      </div>
+      <div class="footer__entry">
+        <h4 class="footer__entry__header">Lavora con noi</h4>
+        <div class="footer__entry__text">
+          Clicca qui per inviare il tuo curriculum
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -11,17 +29,35 @@ export default {};
 <style lang="scss" scoped>
 .footer {
   @include bg-gradient;
-  @include home-wrapper;
+  @include section-wrapper;
 
-  padding-top: 0 !important;
+  padding-top: 30px !important;
   padding-bottom: 0 !important;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 }
 
 .footer__logo {
   width: 180px;
   height: 180px;
+}
+
+.footer__entries {
+  margin-top: 30px;
+}
+.footer__entry {
+  margin-bottom: 30px;
+
+  &__header {
+    font-size: medium;
+    color: white;
+    margin-bottom: 5px;
+  }
+  &__text {
+    @include section-text;
+
+    color: white;
+    line-height: 18px;
+  }
 }
 </style>
