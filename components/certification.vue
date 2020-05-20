2928febdb5a7ad86c383a9ab7a3d9aa41b1ca1b9
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2020-05-20 19:18:33 +0200

    Add certification

---
+++ components/certification.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="certification">
+    <div class="certification__wrapper">
+      <img class="certification__image" src="~assets/images/certification.png" />
+      <div class="certification__main">
+        <h3 class="certification__header">Dielle è un'azienda certificata</h3>
+        <spacer class="certification__spacer" />
+        <div
+          class="certification__text"
+        >Le nostre esperienze e le nostre conoscenze ci permettono di eseguire quadri elettrici e cablaggi nel rispetto delle più severe normative, sia europee (CE) che nord – americane (UL/CSA).</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Spacer from "./common/spacer.vue";
+
+export default {
+  components: {
+    Spacer
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.certification__wrapper {
+  @include section-wrapper;
+
+  display: flex;
+  justify-content: space-between;
+}
+.certification__main {
+  text-align: right;
+}
+.certification__header {
+  @include section-header;
+}
+.certification__spacer {
+  margin-left: auto;
+}
+.certification__text {
+  @include section-text;
+
+  max-width: 580px;
+}
+.certification__image {
+  width: 300px;
+  margin-left: 30px;
+  display: none;
+
+  @include md {
+    display: block;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <navbar></navbar>
     <home></home>
+    <certification></certification>
     <about></about>
     <contact></contact>
     <foo></foo>
@@ -11,6 +12,7 @@
 <script lang="ts">
 import Navbar from "~/components/navigation/navbar.vue";
 import Home from "~/components/home.vue";
+import Certification from "~/components/certification.vue";
 import About from "~/components/about.vue";
 import Contact from "~/components/contact.vue";
 import Footer from "~/components/footer.vue";
@@ -19,6 +21,7 @@ export default {
   components: {
     Navbar,
     Home,
+    Certification,
     About,
     Contact,
     foo: Footer
