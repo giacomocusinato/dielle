bc1b1394216f842258291117b322a430b7550c92
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2020-06-30 17:44:23 +0200

    Improve footer styles

--- components/footer.vue
+++ components/footer.vue
@@ -1,30 +1,33 @@
 <template>
   <footer class="footer">
     <div class="footer__wrapper">
-      <img class="footer__logo" src="~assets/icons/logo-white.svg" />
-      <div class="footer__entries">
-        <div class="footer__entry">
-          <h4 class="footer__entry__header">Contatti</h4>
-          <div class="footer__entry__text">
-            Via Galileo Galilei 1, Galliera Veneta (PD) 35015
-            <br />Tel: 049 9440586
-            <br />Fax: 049 9443414
-            <br />Mail:
-            <a
-              href="mailto:info@dielleimpianti.it"
-              alt="Contattaci via mail"
-            >info@dielleimpianti.it</a>
-            <br />Partita IVA: 02474770282
+      <div class="footer__main">
+        <img class="footer__logo" src="~assets/icons/logo-white.svg" />
+        <div class="footer__entries">
+          <div class="footer__entry">
+            <h4 class="footer__entry__header">Dielle s.r.l</h4>
+            <div class="footer__entry__text">
+              Via Galileo Galilei 1, Galliera Veneta (PD) 35015
+              <br />Tel: 049 9440586
+              <br />Fax: 049 9443414
+              <br />Mail:
+              <a
+                href="mailto:info@dielleimpianti.it"
+                alt="Contattaci via mail"
+              >info@dielleimpianti.it</a>
+              <br />Partita IVA: 02474770282
+            </div>
           </div>
-        </div>
-        <div class="footer__entry">
+          <!-- <div class="footer__entry">
           <h4 class="footer__entry__header">Lavora con noi</h4>
           <div class="footer__entry__text">
             Clicca
             <a href="mailto:info@dielleimpianti.it" alt="Inviaci il curriculum">qui</a> per inviare il tuo curriculum
           </div>
+          </div>-->
         </div>
       </div>
+      <div class="footer__copy">Dielle s.r.l © 2020</div>
     </div>
   </footer>
 </template>
@@ -43,24 +46,40 @@ export default {};
 .footer__wrapper {
   @include section-wrapper;
 
-  padding: 30px;
+  padding: 60px 30px 30px 30px;
+}
+
+.footer__main {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
 }
 
 .footer__logo {
-  width: 180px;
-  height: 180px;
+  width: 120x;
+  height: 120px;
+  margin-right: 15px;
+
+  @include md {
+    margin-right: 0;
+    width: 150px;
+    height: 150px;
+  }
 }
 
 .footer__entries {
-  margin-top: 30px;
+  display: flex;
+  flex-wrap: wrap;
 }
 .footer__entry {
+  flex: 50%;
   margin-bottom: 30px;
 
+  @include md {
+    width: 160px;
+  }
+
   &__header {
-    font-size: medium;
+    font-size: 15px;
     color: white;
     margin-bottom: 5px;
   }
@@ -77,4 +96,17 @@ export default {};
     }
   }
 }
+.footer__copy {
+  @include section-text;
+
+  color: white;
+  font-style: italic;
+  text-align: center;
+  margin-top: 15px;
+
+  @include md {
+    margin-top: 30px;
+  }
+}
 </style>
+
