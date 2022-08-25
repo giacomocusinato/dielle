f9149a06daacb0ff7d2d393bce2b410370b1c01c
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-08-25 19:14:20 +0200

    feat: implement skeleton hero description
    and include some global brand styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { PlusIcon } from '@heroicons/react/solid';
 import { Header } from '../components/Header';
 import { HeroLogo } from '../components/HeroLogo';
 
@@ -22,6 +23,18 @@ const Home: NextPage = () => {
         <HeroLogo />
       </section>
 
+      <section className="container mx-auto py-20 px-8 xl:px-40">
+        <p className="text-4xl text-brand">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Ullam voluptatem ipsum repudiandae sint accusamus deserunt, vitae rem natus enim reprehenderit cumque dolores necessitatibus commodi soluta alias dicta tempore ipsa ab.
+        </p>
+        <a
+          className="mt-8 text-2xl text-brand flex items-center"
+          role="button">
+          <PlusIcon className="w-5 h-5 mr-2" />
+          More
+        </a>
+      </section>
     </>
   )
 }
