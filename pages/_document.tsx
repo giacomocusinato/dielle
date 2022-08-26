b5d7ca3caea981aaa63478efecff5d9decb54ac5
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-08-26 17:52:32 +0200

    feat: introduce next `_document.tsx` and set html lang

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
+import i18nextConfig from '../next-i18next.config'
+
+
+function Document(props: DocumentProps) {
+  const currentLocale =
+    props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
+  const lang = Array.isArray(currentLocale) ? currentLocale[0] : currentLocale;
+
+  return (
+    <Html lang={lang}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document;
