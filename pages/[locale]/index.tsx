b0cdb26e84ed83846df48e3603f7499225be095a
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-08-27 18:03:33 +0200

    feat: implement show more logic in homepage

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { PlusIcon } from '@heroicons/react/solid';
@@ -7,6 +8,8 @@ import { HeroLogo } from '../../components/HeroLogo';
 import { getStaticPaths, makeStaticProps } from '../../lib/getStatic'
 
 const Home: NextPage = () => {
+  const [showMore, setShowMore] = useState(false);
+
   const { t } = useTranslation(['common', 'home']);
 
   return (
@@ -29,12 +32,18 @@ const Home: NextPage = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Ullam voluptatem ipsum repudiandae sint accusamus deserunt, vitae rem natus enim reprehenderit cumque dolores necessitatibus commodi soluta alias dicta tempore ipsa ab.
         </p>
-        <a
+        {showMore ? <p className="mt-8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero numquam expedita, labore ipsam ratione minus eum cumque optio at inventore error fugiat provident commodi? Necessitatibus exercitationem laborum animi officia inventore.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi ipsum quam dolor, dolorum officia earum. Provident, assumenda architecto molestias, est quidem voluptate, incidunt iusto recusandae aliquam nesciunt ex voluptas doloremque?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dolorum iste at ea, assumenda, nam consequatur odit nihil eum eius labore possimus. Harum accusamus, quaerat officia tempore quia temporibus soluta.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, dolorum iste at ea, assumenda, nam consequatur odit nihil eum eius labore possimus. Harum accusamus, quaerat officia tempore quia temporibus soluta.
+        </p> : <a
           className="mt-8 text-2xl text-brand flex items-center"
-          role="button">
+          role="button"
+          onClick={() => setShowMore(true)}>
           <PlusIcon className="w-5 h-5 mr-2" />
           {t("home:showMore")}
-        </a>
+        </a>}
       </section>
     </>
   )
