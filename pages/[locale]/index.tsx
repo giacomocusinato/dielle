2b06b9308a91f08cb366ebfd6df3592a42b2b2ea
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-10-17 15:29:19 +0200

    feat: add about us section

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -48,6 +48,41 @@ const Home: NextPage = () => {
       </section>
 
       <SectionServices />
+
+      <section className="relative">
+        <div className="container pt-20 lg:px-[180px] xl:px-[240px]">
+          <h2 className="mb-16 text-4xl text-medium sm:text-center">
+            Su di <span className="text-dielle">noi</span>
+          </h2>
+          <div className="space-y-12">
+            <div className="flex max-w-4xl">
+              <img src="/ul-certification.svg" className="w-20 flex-shrink-0" />
+              <div className="ml-8">
+                <h3 className="mb-1 text-xl font-semibold">UL Certification</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, laborum. Dolor necessitatibus dolores doloribus maxime, minima laborum maiores hic vitae commodi at laboriosam, modi impedit dolorem neque eos labore. Quasi?</p>
+              </div>
+            </div>
+
+            <div className="flex max-w-4xl ml-auto">
+              <div className="mr-8 text-right">
+                <h3 className="mb-1 text-xl font-semibold">Supporto cliente</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, laborum. Dolor necessitatibus dolores doloribus maxime, minima laborum maiores hic vitae commodi at laboriosam, modi impedit dolorem neque eos labore. Quasi?</p>
+              </div>
+              <img src="/client-support.svg" className="w-20 flex-shrink-0" />
+            </div>
+
+            <div className="flex max-w-4xl">
+              <img src="/industry-4.svg" className="w-20 flex-shrink-0" />
+              <div className="ml-8">
+                <h3 className="mb-1 text-xl font-semibold">Industria 4.0</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, laborum. Dolor necessitatibus dolores doloribus maxime, minima laborum maiores hic vitae commodi at laboriosam, modi impedit dolorem neque eos labore. Quasi?</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      )
     </>
   )
 }
