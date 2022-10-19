92ec836087bdcde09cb53337d0fc970556f1e356
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-10-19 21:22:47 +0200

    feat: add button sizes and types

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,6 +4,9 @@ import React, { MouseEventHandler } from "react";
 
 type ButtonProps = {
   href?: string,
+  size?: 'md' | 'lg';
+  rounded?: boolean;
+  stroked?: boolean;
   children?: React.ReactNode
   className?: string;
   onClick?: MouseEventHandler<HTMLAnchorElement>,
@@ -11,21 +14,30 @@ type ButtonProps = {
 
 const defaultProps: ButtonProps = {
   href: '#',
+  size: 'md',
+  rounded: false,
+  stroked: false,
   onClick: () => { },
 }
 
-export const Button: React.FC<ButtonProps> = ({ href, onClick, className, children }) => {
+export const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
   return (
     <a
       className={classNames(
-        'flex items-center justify-center px-5 py-3 w-fit bg-dielle text-white text-medium rounded-sm',
+        'flex items-center justify-center w-fit font-medium',
         'hover:opacity-90 transition-all duration-175',
+        props.stroked ? 'outline outline-2 outline-dielle text-dielle' : 'bg-dielle text-white',
+        props.rounded ? 'rounded-[40px]' : 'rounded-sm',
+        props.size === 'md' && 'py-[6px] px-4',
+        props.size === 'lg' && 'py-3 px-5',
         className,
       )}
-      role={href ? 'link' : 'button'}
-      href={href}
-      onClick={onClick}>
+      role={props.href ? 'link' : 'button'}
+      href={props.href}
+      onClick={props.onClick}>
       {children}
     </a>
   );
 }
+
+Button.defaultProps = defaultProps;
