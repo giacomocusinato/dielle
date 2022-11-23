9617a4cd8beb86b5775821ef50c9f9be7d78a978
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-11-23 17:09:09 +0100

    feat: uniform alignment in traits section

--- components/Traits/Traits.tsx
+++ components/Traits/Traits.tsx
@@ -5,11 +5,12 @@ export const Traits = () => {
   const { t } = useTranslation(['common']);
 
   return (
-    <section id="traits">
-      <div className="container py-10 sm:py-20 lg:px-[180px] xl:px-[240px]">
+    <section id="traits" className="relative">
+      <div className="container py-10 sm:py-40 lg:px-[180px] xl:px-[240px]">
         <h2 className="mb-10 sm:mb-16 text-4xl text-medium text-center">
           Su di <span className="text-dielle">noi</span>
         </h2>
+
         <div className="space-y-12">
           <div className="flex max-w-4xl">
             <img src="/ul-certification.svg" className="w-20 flex-shrink-0 self-start" />
@@ -19,12 +20,12 @@ export const Traits = () => {
             </div>
           </div>
 
-          <div className="flex max-w-4xl ml-auto">
-            <div className="mr-8 text-right">
+          <div className="flex max-w-4xl">
+            <img src="/client-support.svg" className="w-20 flex-shrink-0 self-start" />
+            <div className="ml-8">
               <h3 className="mb-1 text-xl font-semibold">Supporto cliente</h3>
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, laborum. Dolor necessitatibus dolores doloribus maxime, minima laborum maiores hic vitae commodi at laboriosam, modi impedit dolorem neque eos labore. Quasi?</p>
             </div>
-            <img src="/client-support.svg" className="w-20 flex-shrink-0 self-start" />
           </div>
 
           <div className="flex max-w-4xl">
@@ -34,8 +35,8 @@ export const Traits = () => {
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, laborum. Dolor necessitatibus dolores doloribus maxime, minima laborum maiores hic vitae commodi at laboriosam, modi impedit dolorem neque eos labore. Quasi?</p>
             </div>
           </div>
-
         </div>
+
       </div>
     </section>
   );
