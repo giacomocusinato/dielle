7a8bc09b422d3ee037b636894714849362ad8566
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-11-23 15:22:03 +0100

    feat: redesign industries section

--- components/Industries/Industries.tsx
+++ components/Industries/Industries.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Image from 'next/image';
-import classNames from 'classnames';
 import { useTranslation } from 'next-i18next';
+import { ArrowRightIcon } from '@heroicons/react/solid';
+import { Button } from '../Button';
 
 type IndustryItemProps = {
   img: string;
@@ -12,21 +12,12 @@ type IndustryItemProps = {
 
 export const IndustryItem: React.FC<IndustryItemProps> = (props: IndustryItemProps) => {
   return (
-    <div className={classNames(
-      'flex items-end', { 'flex-row-reverse text-right': props.reverse })
-    }>
-      <Image
-        height={200}
-        width={300}
-        src={props.img}
-        alt={props.title}
-      />
-      <div className={classNames('max-w-2xl', { 'mr-8': props.reverse, 'ml-8': !props.reverse, })}>
-        <h3 className="text-xl font-semibold">{props.title}</h3>
-        <p className="mt-1 text-sm">{props.description}</p>
-      </div>
+    <div>
+      <img className="rounded" src={props.img} alt={props.title} />
+      <h3 className="text-lg font-medium mt-1">{props.title}</h3>
+      <p>{props.description}</p>
     </div>
-  )
+  );
 };
 
 export const Industries = () => {
@@ -34,37 +25,44 @@ export const Industries = () => {
 
   return (
     <section id="industries">
-      <div className="container py-10 sm:py-20">
-        <h2 className="mb-16 text-4xl text-medium sm:text-center">
+      <div className="py-10 sm:py-20">
+        <h2 className="mb-10 sm:mb-16 text-4xl text-medium text-center">
           {t('industries.title1')} <span className="text-dielle">{t('home:industries.title2')}</span>
         </h2>
-        <div className="mt-8 space-y-8 relative">
-          <div className="absolute top-20 left-20 bottom-32 right-20 border border-dielle -z-10" aria-hidden="true"></div>
-          <IndustryItem
-            img="/industries/pasta.jpg"
-            title={t('home:industries.food')}
-            description={t('home:industries.foodDesc')} />
-          <IndustryItem
-            reverse
-            img="/industries/packaging.jpg"
-            title={t('home:industries.packaging')}
-            description={t('home:industries.packagingDesc')} />
-          <IndustryItem
-            img="/industries/boat-lifter.jpg"
-            title={t('home:industries.boat')}
-            description={t('home:industries.boatDesc')} />
-          <IndustryItem
-            reverse={true}
-            img="/industries/marble.jpg"
-            title={t('home:industries.marble')}
-            description={t('home:industries.marbleDesc')} />
-          <IndustryItem
-            img="/industries/metal.jpg"
-            title={t('home:industries.metal')}
-            description={t('home:industries.metalDesc')} />
+
+        <div className=" py-20">
+          <div className="container">
+            <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+              <IndustryItem
+                img="/industries/pasta.jpg"
+                title={t('home:industries.food')}
+                description={t('home:industries.foodDesc')} />
+              <IndustryItem
+                reverse
+                img="/industries/packaging.jpg"
+                title={t('home:industries.packaging')}
+                description={t('home:industries.packagingDesc')} />
+              <IndustryItem
+                img="/industries/boat-lifter.jpg"
+                title={t('home:industries.boat')}
+                description={t('home:industries.boatDesc')} />
+              <IndustryItem
+                reverse={true}
+                img="/industries/marble.jpg"
+                title={t('home:industries.marble')}
+                description={t('home:industries.marbleDesc')} />
+              <IndustryItem
+                img="/industries/metal.jpg"
+                title={t('home:industries.metal')}
+                description={t('home:industries.metalDesc')} />
+            </div>
+            <Button className="mt-16" rounded stroked size="lg" href="/contact">
+              Scopri di più
+              <ArrowRightIcon className="w-5 ml-4" />
+            </Button>
+          </div>
         </div>
       </div>
-      <img src="/cables.jpg" className="w-full mt-4 sm:hidden " />
     </section>
   )
 };
