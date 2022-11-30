e62f0f6018285da9cf19d7ea238142e50cb75db0
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2022-11-30 17:39:53 +0100

    feat: refactor button component
    better differentiate between Links and button and use next `Link`

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React, { MouseEventHandler } from 'react';
 import classNames from 'classnames';
+import Link from 'next/link';
 
 type ButtonProps = {
   href?: string,
@@ -8,7 +9,7 @@ type ButtonProps = {
   stroked?: boolean;
   children?: React.ReactNode
   className?: string;
-  onClick?: MouseEventHandler<HTMLAnchorElement>,
+  onClick?: MouseEventHandler<HTMLButtonElement>,
 };
 
 const defaultProps: ButtonProps = {
@@ -19,24 +20,23 @@ const defaultProps: ButtonProps = {
   onClick: () => { },
 }
 
-export const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
-  return (
-    <a
-      className={classNames(
-        'flex items-center justify-center w-fit font-medium',
-        'hover:opacity-90 transition-all duration-175',
-        props.stroked ? 'outline outline-2 outline-dielle text-dielle' : 'bg-dielle text-white',
-        props.rounded ? 'rounded-[40px]' : 'rounded-sm',
-        props.size === 'md' && 'py-[6px] px-4',
-        props.size === 'lg' && 'py-3 px-5',
-        className,
-      )}
-      role={props.href ? 'link' : 'button'}
-      href={props.href}
-      onClick={props.onClick}>
-      {children}
-    </a>
+export const Button: React.FC<ButtonProps> = ({ className, children, href, onClick, ...props }) => {
+  const classes = classNames(
+    'flex items-center justify-center w-fit font-medium',
+    'hover:opacity-90 transition-all duration-175',
+    props.stroked ? 'outline outline-2 outline-dielle text-dielle' : 'bg-dielle text-white',
+    props.rounded ? 'rounded-[40px]' : 'rounded-sm',
+    props.size === 'md' && 'py-[6px] px-4',
+    props.size === 'lg' && 'py-3 px-5',
+    className,
   );
+
+  return href ? (
+    <Link href={href}>
+      <a className={classes}>{children}</a>
+    </Link>
+  ) :
+    <button onClick={onClick} className={classes}>{children}</button>
 }
 
 Button.defaultProps = defaultProps;
