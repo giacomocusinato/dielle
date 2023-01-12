db42f280906a6de3007135663f3703324f00104f
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2023-01-12 20:01:26 +0100

    feat: introduce page header with image handling

---
+++ components/PageHeader/PageHeader.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const PageHeader: React.FC<{ imgSrc: string, imgPosition?: number, title: string }> = ({ imgSrc, imgPosition = 0, title }) => {
+  return (
+    <div className="relative overflow-hidden h-[280px]">
+      <img src={imgSrc} className="absolute w-full h-full object-cover -z-10" style={{ objectPosition: `0 ${imgPosition}%` }} />
+      <div className="absolute bg-dielle/80 inset-0 -z-10" />
+
+      <div className="absolute w-0 h-0 bottom-0 border-b-[50px] border-r-[100vw] border-transparent border-b-white" />
+
+      <h1 className="text-5xl text-medium text-center mt-24 text-white drop-shadow">
+        {title}
+      </h1>
+    </div>
+
+  );
+}
