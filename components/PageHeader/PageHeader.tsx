83c8b59e54a890ec4ffebe2f5efc4300161bf7ff
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2023-01-12 21:51:17 +0100

    feat: update `PageHeader`

--- components/PageHeader/PageHeader.tsx
+++ components/PageHeader/PageHeader.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-export const PageHeader: React.FC<{ imgSrc: string, imgPosition?: number, title: string }> = ({ imgSrc, imgPosition = 0, title }) => {
+export const PageHeader: React.FC<{ imgSrc: string, imgPosition?: number, title?: string }> = ({ imgSrc, imgPosition = 0, title }) => {
   return (
-    <div className="relative overflow-hidden h-[280px]">
+    <div className="relative overflow-hidden h-[340px]">
       <img src={imgSrc} className="absolute w-full h-full object-cover -z-10" style={{ objectPosition: `0 ${imgPosition}%` }} />
       <div className="absolute bg-dielle/80 inset-0 -z-10" />
+      <div className="absolute w-0 h-0 bottom-0 border-b-[50px] border-r-[100vw] border-transparent border-b-white -z-10" />
 
-      <div className="absolute w-0 h-0 bottom-0 border-b-[50px] border-r-[100vw] border-transparent border-b-white" />
-
-      <h1 className="text-5xl text-medium text-center mt-24 text-white drop-shadow">
+      {title && <h1 className="text-5xl text-medium text-center mt-32 text-white drop-shadow">
         {title}
-      </h1>
+      </h1>}
     </div>
 
   );
