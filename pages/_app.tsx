6d2df201fefd2450e310d49b05a79ae5f5a64dab
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2023-01-15 15:21:32 +0100

    feat: implement cookie consumer in app root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type { AppProps } from 'next/app'
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation, useTranslation } from 'next-i18next'
+import { CookiesProvider } from '../components/CookieBanner/CookiesContext';
+import { CookiesRootConsumer } from '../components/CookieBanner/CookiesRootConsumer';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CookiesProvider>
+      <CookiesRootConsumer />
+      <Component {...pageProps} />
+    </CookiesProvider>
+  )
 }
+
 
 export default appWithTranslation(MyApp);
