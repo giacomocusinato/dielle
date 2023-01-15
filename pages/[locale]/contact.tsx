5d8adfdb97b982798c2dee610968e5ecb6b45605
Giacomo Cusinato <giacomo.cusinato@outlook.com>
2023-01-15 19:21:57 +0100

    feat: implement contact form logic and ux

--- pages/[locale]/contact.tsx
+++ pages/[locale]/contact.tsx
@@ -1,19 +1,55 @@
+import { useState } from 'react';
 import type { NextPage } from 'next'
 import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { LocationMarkerIcon, MailIcon } from "@heroicons/react/solid";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { LocationMarkerIcon, MailIcon, CheckIcon, ExclamationCircleIcon } from "@heroicons/react/solid";
 import { Head } from '../../components/Head';
 import { PageLayout } from '../../components/PageLayout';
 import { PageHeader } from '../../components/PageHeader';
 import { Button } from '../../components/Button';
 import { getStaticPaths, makeStaticProps } from '../../lib/getStatic'
+import classNames from 'classnames';
+import { Spinner } from '../../components/Spinner';
 
 const inputClasses = 'py-3 px-4 w-full outline outline-1 outline-gray-300 focus:outline-dielle rounded-sm';
 
-const Home: NextPage = () => {
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber?: string;
+  message: string;
+  privacy: boolean;
+};
+
+const Contact: NextPage = () => {
   const { t } = useTranslation(['common', 'contact']);
   const router = useRouter();
+
+  const { register, handleSubmit, formState: { isValid, isSubmitted, errors } } = useForm<FormValues>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPopup, setShowPopup] = useState<null | 'success' | 'error'>(null);
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    try {
+      setIsLoading(true);
+      await fetch("/cgi-bin/mailer.php", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      setShowPopup('success');
+    } catch {
+      setShowPopup('error')
+    } finally {
+      setIsLoading(false)
+      setTimeout(() => setShowPopup(null), 4000);
+    }
+  }
 
   return (
     <PageLayout>
@@ -46,36 +82,94 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <form className="mt-10 w-full space-y-4 sm:space-y-5">
+          <form className="mt-10 w-full space-y-4 sm:space-y-5" onSubmit={handleSubmit(onSubmit)}>
             <div className="sm:flex w-full space-y-4 sm:space-x-5 sm:space-y-0">
-              <input className={inputClasses} type="text" placeholder={t('contact:form:firstName')} />
-              <input className={inputClasses} type="text" placeholder={t('contact:form:lastName')} />
+              <input
+                className={classNames(inputClasses, { '!outline-red-500': errors.firstName })}
+                type="text"
+                placeholder={t('contact:form:firstName')}
+                {...register('firstName', { required: true })}
+              />
+              <input
+                className={classNames(inputClasses, { '!outline-red-500': errors.lastName })}
+                type="text"
+                placeholder={t('contact:form:lastName')}
+                {...register('lastName', { required: true })}
+              />
             </div>
             <div className="sm:flex w-full space-y-4 sm:space-x-5 sm:space-y-0">
-              <input className={inputClasses} type="text" placeholder={t('contact:form:email')} />
-              <input className={inputClasses} type="text" placeholder={t('contact:form:phoneNumber')} />
+              <input
+                className={classNames(inputClasses, { '!outline-red-500': errors.email })}
+                type="text"
+                placeholder={t('contact:form:email')}
+                {...register('email', {
+                  required: true,
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Entered value does not match email format"
+                  }
+                })}
+              />
+              <input
+                className={inputClasses}
+                type="text"
+                placeholder={t('contact:form:phoneNumber')}
+                {...register('phoneNumber')}
+              />
             </div>
-            <textarea rows={5} className={inputClasses} placeholder={t('contact:form:text')}></textarea>
+            <textarea
+              rows={5}
+              className={classNames(inputClasses, { '!outline-red-500': errors.message })}
+              placeholder={t('contact:form:text')}
+              {...register('message', { required: true })}
+            ></textarea>
 
             <div className="flex space-x-2">
-              <input type="checkbox" className="accent-dielle cursor-pointer"></input>
-              <p className="font-light text-sm">
+              <input
+                type="checkbox"
+                className="accent-dielle cursor-pointer "
+                {...register('privacy', { required: true })}
+              ></input>
+              <p className="font-light text-sm relative">
                 {t('contact:form:privacy1')}{' '}
                 <Link href={`/${router.query.locale}/privacy`}>
                   <a target="_blank" className="text-dielle font-medium">{t('contact:form:privacy2')}</a>
-                </Link>.
+                </Link>
+                {errors.privacy && <span className="absolute text-lg text-red-500 -right-4 -top-1">*</span>}
               </p>
             </div>
 
-            <Button size="lg" className="!w-full">Submit</Button>
+            <div className="text-sm text-center text-red-600 h-6 mt-3">
+              {!isValid && isSubmitted && t('contact:form:error')}
+            </div>
+
+            <Button size="lg" className="!w-full" type="submit">
+              {isLoading ? <Spinner /> : t('contact:form:submit')}
+            </Button>
           </form>
+
+          {showPopup &&
+            <div className={classNames('fixed right-8 bottom-8 px-5 py-3 text-white rounded-sm shadow-sm flip-in', {
+              'bg-dielle': showPopup == 'success',
+              'bg-red-800': showPopup == 'error',
+            })}>
+              <h4 className="flex items-center mb-1">
+                {showPopup === 'success'
+                  ? <CheckIcon className="w-5 h-5 mr-2" />
+                  : <ExclamationCircleIcon className="w-5 h-5 mr-2" />}
+                {showPopup === 'success' ? t(`contact:successTitle`) : t(`contact:errorTitle`)}
+              </h4>
+              <p className="text-sm font-light">
+                {showPopup === 'success' ? t(`contact:successMessage`) : t(`contact:errorMessage`)}
+              </p>
+            </div>}
         </div>
       </section>
     </PageLayout>
   )
 }
 
-export default Home;
+export default Contact;
 
 const getStaticProps = makeStaticProps(['common', 'head', 'header', 'footer', 'cookies', 'contact',]);
 export { getStaticPaths, getStaticProps };
